483e054955ad955cbe7c1f1205f7a64cd57a5b7a
jiyu <giu0213@naver.com>
2019-04-24 20:44:43 +0900

    asdf

--- ui/src/views/document/Board.vue
+++ ui/src/views/document/Board.vue
@@ -4,14 +4,14 @@
       id="title"
       v-model="model.title"
       rows="1"
-      placeholder="제목"
-    >{{text.title}}</b-form-textarea>
+    
+    />
     <br>
     <b-form-textarea
       id="content"
       v-model="model.content"
       rows="8"
-      :placeholder="text.content"   
+      :placeholder="text.content"  
       />
     <br>
     <div style="float:right;">
@@ -38,9 +38,9 @@ export default {
   mounted: async function () {
     console.log('api', api)
     const rs = await api.document.get();
-    this.items = rs.data;
-
-    this.items.map(item => {
+    this.model = rs.data;
+    console.log(this.model)
+    this.model.map(item => {
       if(item.id.toString()===this.$route.params.id){
         this.text = item
       }
